9c5f08eb986e3736eaaa49f34ccea6b9bf10ce7a
philippzach <mail@philippzach.com>
2019-03-01 15:12:09 -1000

    og:imag fix

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -55,13 +55,13 @@ const Head = ({
     <meta content={canonical} name="twitter:url" />
     {/* <link rel="canonical" href={canonical} /> */}
 
-    <meta content={`${siteUrl}/static/social.jpg`} property="og:image" />
+    <meta content={`${siteUrl}/social.jpg`} property="og:image" />
     <meta content="1024" property="og:image:width" />
     <meta content="512" property="og:image:height" />
-    <meta content={`${siteUrl}/static/social.jpg`} name="twitter:image" />
+    <meta content={`${siteUrl}/social.jpg`} name="twitter:image" />
     <meta content="1024" name="twitter:image:width" />
     <meta content="512" name="twitter:image:height" />
-    <meta content={`${siteUrl}/static/social.jpg`} property="og:image" />
+    <meta content={`${siteUrl}/social.jpg`} property="og:image" />
     <meta content="1024" property="og:image:width" />
     <meta content="512" property="og:image:height" />
 
